9031ff41a4f228bfe627c33f1bf6b96181c3647b
sakamot <matuhana.8@gmail.com>
2019-01-26 18:56:47 +0900

    Initial Commit

---
+++ takeBarcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  takeBarcode
+//
+//  Created by sakamoto on 2019/01/26.
+//  Copyright © 2019 sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
