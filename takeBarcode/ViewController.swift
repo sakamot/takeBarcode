a72a9ab17840ef84943cad9375044c2e014e0433
sakamot <matuhana.8@gmail.com>
2019-01-27 23:19:33 +0900

    first commit 🎉

--- takeBarcode/ViewController.swift
+++ takeBarcode/ViewController.swift
@@ -7,14 +7,105 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var captureView: UIView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    private lazy var captureSession: AVCaptureSession = AVCaptureSession()
+    private lazy var captureDevice: AVCaptureDevice = AVCaptureDevice.default(for: AVMediaType.video)!
+    private lazy var capturePreviewLayer: AVCaptureVideoPreviewLayer = {
+        let layer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        return layer
+    }()
+    
+    private var captureInput: AVCaptureInput? = nil
+    private lazy var Output: AVCaptureMetadataOutput = {
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: .main)
+        return output
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+
+        setupBarcodeCapture()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        capturePreviewLayer.frame = self.captureView?.bounds ?? CGRect.zero
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - private
+    private func setupBarcodeCapture() {
+        do {
+            captureInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureInput!)
+            captureSession.addOutput(Output)
+            Output.metadataObjectTypes = Output.availableMetadataObjectTypes
+            capturePreviewLayer.frame = self.captureView?.bounds ?? CGRect.zero
+            capturePreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            captureView?.layer.addSublayer(capturePreviewLayer)
+            captureSession.startRunning()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    private func convartISBN(value: String) -> String? {
+        let v = NSString(string: value).longLongValue
+        let prefix: Int64 = Int64(v / 10000000000)
+        guard prefix == 978 || prefix == 979 else { return nil }
+        let isbn9: Int64 = (v % 10000000000) / 10
+        var sum: Int64 = 0
+        var tmpISBN = isbn9
+        
+        var i = 10
+        while i > 0 && tmpISBN > 0 {
+            let divisor: Int64 = Int64(pow(10, Double(i - 2)))
+            sum += (tmpISBN / divisor) * Int64(i)
+            tmpISBN %= divisor
+            i -= 1
+        }
+        
+        let checkdigit = 11 - (sum % 11)
+        return String(format: "%lld%@", isbn9, (checkdigit == 10) ? "X" : String(format: "%lld", checkdigit % 11))
     }
 
-
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        self.captureSession.stopRunning()
+        let objects = metadataObjects
+        var detectionString: String? = nil
+        let barcodeTypes = [AVMetadataObject.ObjectType.ean8, AVMetadataObject.ObjectType.ean13]
+        for metadataObject in objects {
+            loop: for type in barcodeTypes {
+                guard metadataObject.type == type else { continue }
+                guard self.capturePreviewLayer.transformedMetadataObject(for: metadataObject) is AVMetadataMachineReadableCodeObject else { continue }
+                if let object = metadataObject as? AVMetadataMachineReadableCodeObject {
+                    detectionString = object.stringValue
+                    break loop
+                }
+            }
+            var text = ""
+            guard let value = detectionString else { continue }
+            text += "読み込んだ値:\t\(value)"
+            guard let isbn = convartISBN(value: value) else { continue }
+            resultLabel.text = text
+            let URLString = String(format: "http://amazon.co.jp/dp/%@", isbn)
+            guard let URL = NSURL(string: URLString) else { continue }
+            UIApplication.shared.open(URL as URL)
+        }
+        self.captureSession.startRunning()
+    }
 }
-
